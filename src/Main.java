4ce5f11ce8ee4928d447ac9388ba6637f3aee864
codymemeric <nice.kutenkov@mail.ru>
2024-07-11 21:20:03 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Введите первое число");
+        double a = new Scanner(System.in).nextDouble();
+
+        System.out.println("Введите второе число");
+        double b = new Scanner(System.in).nextDouble();
+
+        System.out.println("- " + (a - b));
+        System.out.println("+ " + (a + b));
+        System.out.println("/ " + (a / b));
+        System.out.println("* " + (a * b));
+
+
+
+
+
+    }
+}
